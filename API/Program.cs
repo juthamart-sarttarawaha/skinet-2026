70d895fd680e666fb02eaf98c30f290f14e7321e
juthamart-sarttarawaha <juthamart.sartta@gmail.com>
2026-03-28 13:20:16 +0700

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
