4c2ca8da53e0a33231373ad989aefb761d296260
juthamart-sarttarawaha <juthamart.sartta@gmail.com>
2026-03-29 05:58:56 +0700

    End of section 7

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  // protected readonly title = signal('client');
+  title = 'client';
+}
