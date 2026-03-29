e17d5c0be7ee56742e91071dc0ea3d328f7c118d
juthamart-sarttarawaha <juthamart.sartta@gmail.com>
2026-03-29 23:04:47 +0700

    End of section 10

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -5,7 +5,7 @@ import { Product } from '../../shared/models/product';
 import { ShopParams } from '../../shared/models/shopParams';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
@@ -25,31 +25,36 @@ export class ShopService {
     }
 
     if (shopParams.sort) {
-      params = params.append('sort', shopParams.sort)
+      params = params.append('sort', shopParams.sort);
     }
 
     if (shopParams.search) {
-      params = params.append('search', shopParams.search)
+      params = params.append('search', shopParams.search);
     }
 
     params = params.append('pageSize', shopParams.pageSize);
     params = params.append('pageIndex', shopParams.pageNumber);
 
-    return this.http.get<Pagination<Product>>(this.baseUrl + 'products', { params });
+    return this.http.get<Pagination<Product>>(this.baseUrl + 'products', {
+      params,
+    });
   }
 
-getBrands() {
+  getProduct(id:number) {
+    return this.http.get<Product>(this.baseUrl + 'products/'+id);
+  }
+
+  getBrands() {
     if (this.brands.length > 0) return;
     return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
-      next: response => this.brands = response,
-    })
+      next: (response) => (this.brands = response),
+    });
   }
 
   getTypes() {
     if (this.types.length > 0) return;
     return this.http.get<string[]>(this.baseUrl + 'products/types').subscribe({
-      next: response => this.types = response,
-    })
+      next: (response) => (this.types = response),
+    });
   }
-
 }
